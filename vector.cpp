f687be66f6d8b592ec6f7588b4b7099dfbde9750
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-05-21 01:21:53 +0600

    start

---
+++ vector.cpp
@@ -0,0 +1,28 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+    vector<int>a ={1,2,3,4,5};
+    a.clear();
+    cout<<a.empty();
+
+    return 0;
+}
