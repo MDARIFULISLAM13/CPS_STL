8da6ad2ab9d8098c58eebfc75b2c2a027250096b
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-07-09 23:09:35 +0600

    stl

--- vector.cpp
+++ vector.cpp
@@ -13,17 +13,22 @@ using ll = long long int;
 #define du double
 #define ull unsigned long long
 #define vec vector<ll>
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    vector<int>a;
-a.resize(3,5 );
-vec b ;
-b.resize(5,1);
-for(int i:b ){
-  cout<<i<<endl;
-}
-    return 0;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  vector<int>a;
+  a.resize(3, 5);
+  vec b;
+  b.resize(5, 1);
+  vector<int>::iterator at;
+  for (at = a.begin(); at != a.end();at++) {
+    cout << *at << endl;
+  }
+  for (auto i : a)cout << i << endl;
+  for (int i : b) {
+    cout << i << endl;
+  }
+  return 0;
 }
